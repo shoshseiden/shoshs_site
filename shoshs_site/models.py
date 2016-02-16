59192dc753a76a74c971bc86b2c231f5c2ae2a3f
Shosh Seiden <shosh.j.seiden@gmail.com>
2016-02-16 12:35:46 -0700

    created graphic and web models

---
+++ shoshs_site/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Website(models.Model):
+    website_name = models.CharField(max_length=25)
+    website_url = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.website_name
+
+
+class Graphic(models.Model):
+    graphic_image = models.ImageField(upload_to="graphic_images")
+    graphic_image_description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.graphic_image_description
