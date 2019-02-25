a05b1c20a3ab78e5d14d81eafe3fc11b1ee11828
Shosh Seiden <shosh.j.seiden@gmail.com>
2019-02-24 20:42:57 -0700

    added website_screenshot attribute

--- shoshs_site/admin.py
+++ shoshs_site/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Website, Graphic
 
 admin.site.register(
-    Website, list_display=["website_url", "website_name"]
+    Website, list_display=["website_url", "website_name", "website_screenshot"]
 )
 
 admin.site.register(
--- shoshs_site/models.py
+++ shoshs_site/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Website(models.Model):
     website_name = models.CharField(max_length=25)
     website_url = models.CharField(max_length=100)
+    website_screenshot = models.ImageField(upload_to="website_images")
 
     def __str__(self):
         return self.website_name
